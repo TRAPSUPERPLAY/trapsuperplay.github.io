4edd095b0c7148f1248ed2be127a91276d1ae1c3
TRAPSUPERPLAY <96630479+TRAPSUPERPLAY@users.noreply.github.com>
2022-08-23 16:52:53 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
+function locate_assign() {
+	document.location.assign("https://trapsuperplay.github.io/");
+	}
+	let header__burger = document.querySelector('.header__burger');
+	let header__menu = document.querySelector('.header__menu');
+	
+	header__burger.addEventListener('click', function(){
+		header__burger.classList.toggle('active');
+		header__menu.classList.toggle('active');
+	})
 
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
+	function locate_contact() {
+		document.location.assign("https://docs.google.com/forms/d/e/1FAIpQLScdEuf97O2y0xlMHPdr56-xIXzHCWwca0M--LB8NImD8p9tmw/viewform?usp=sf_link");
+	}
+	function contanting()
+	{
+		document.location.assign("contact/");
+	}
