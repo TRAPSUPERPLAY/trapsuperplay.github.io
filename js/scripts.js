39e7e15acb08c44ab8965cfde855c1ee80eb860b
TRAPSUPERPLAY <96630479+TRAPSUPERPLAY@users.noreply.github.com>
2023-01-13 14:47:00 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,24 @@ function locate_assign() {
 function sends() {
 	alert("But before you send, you need to pass the captcha.");
 }
+function adclick() {
+	document.location.assign("https://trapsuperplay.creator-spring.com/")
+}
+
+function validate(){
+	let form = document.getElementById('form');
+	let formEmail = document.getElementById('formEmail').value;
+	let textvalid = document.getElementById('textvalid');
+	let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+	if(formEmail.match(pattern))
+	{
+		textvalid.innerHTML = "Your Email Address is Valid!";
+		textvalid.style.color = "#4caf50";
+	}
+	else
+	{
+		textvalid.innerHTML = "Please Enter Valid Email Address.";
+		textvalid.style.color = "#f44336";
+	}
+}
